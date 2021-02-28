fe4b732b5d6e0247aea1b21091ff898a77f0e9c5
Maks <maxim.bezliepkin@gmail.com>
2021-02-28 23:57:39 +0200

    trying to write persent function. failed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
 "use strict";
 
 function insert(symbol){
-    document.form.textView.value = document.form.textView.value + symbol;
+    document.form.textView.value = document.form.textView.value + symbol;// закончил на 28:39, сделал кнопки, теперь нужно чтобы не применялось несколько знаков
 }
 
 function clean(){
     document.form.textView.value = '';
 }
 function equal(){
-    document.form.textView.value = eval(document.form.textView.value);
+    document.form.textView.value = eval(document.form.textView.value);  
 }
+// function percent(){
+//     document.form.textView.value = ХЗ
+// }
 
